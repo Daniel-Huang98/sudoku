af7f2134502d0ed3d81a924e7cbed08d98297ef5
Daniel Huang <Daniel.Huang@smash.org.au>
2020-02-23 02:11:17 +1100

    Updates

---
+++ precache-manifest.8f6d8cd0e20fa20b86ff3cc5b48255c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae3976695f6921acea74a111a2e747c",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "5691ab0f3d14f88242cc",
+    "url": "/sudoku/static/css/main.ba93884d.chunk.css"
+  },
+  {
+    "revision": "1be11cff9fc53d3e8c76",
+    "url": "/sudoku/static/js/2.955f612e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sudoku/static/js/2.955f612e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5691ab0f3d14f88242cc",
+    "url": "/sudoku/static/js/main.1ecd862f.chunk.js"
+  },
+  {
+    "revision": "80fdc32de6d895578ae2",
+    "url": "/sudoku/static/js/runtime-main.c8e770ab.js"
+  }
+]);
